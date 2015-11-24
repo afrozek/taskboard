cc49a99a87055d28b80eccc9ad24d3f81a6c2b8f
Afroze Khan <auk2@njit.edu>
2015-11-24 18:36:11 -0500

    added john papa sample service

--- public/components/sampleComponent/sample.service.js
+++ public/components/sampleComponent/sample.service.js
@@ -1,2 +1,40 @@
-sampleService
+(function(){
+	'use strict'
 
+	angular
+    	.module('sample')
+    	.factory('sampleService', sampleService);
+
+    sampleService.inject = ['']
+
+    function sampleService() {
+    	var service = {
+
+    		error: error,
+    		info: info,
+    		success: success
+
+    	};
+
+    	return service;
+
+    	////////////
+
+    	function error() {
+	      /* */
+	    }
+
+	    function info() {
+	      /* */
+	    }
+
+	    function success() {
+	      /* */
+	    }
+
+
+    }
+
+	
+
+})()

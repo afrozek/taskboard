d3ce7b760ce89073547762877f15c4263d480291
Afroze Khan <auk2@njit.edu>
2015-11-24 18:25:09 -0500

    added johnpapa sample controller

--- public/components/sampleComponent/sample.controller.js
+++ public/components/sampleComponent/sample.controller.js
@@ -1,1 +1,37 @@
-sampleController
+(function () {
+	'use strict'
+
+	angular
+		.module('sample')
+		.controller('sample', sample)
+
+	sample.inject = ['']
+
+	function sample() {
+		
+	    var vm = this;
+
+	    vm.gotoSession = gotoSession;
+	    vm.refresh = refresh;
+	    vm.search = search;
+	    vm.sessions = [];
+	    vm.title = 'Sample';
+
+	    ////////////
+
+	    function gotoSession() {
+	      /* */
+	    }
+
+	    function refresh() {
+	      /* */
+	    }
+
+	    function search() {
+	      /* */
+	    }
+	}
+
+})()
+
+

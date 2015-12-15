e9921c0fde49c790acb8f1023c95c2ca9fef5960
Afroze Khan <auk2@njit.edu>
2015-12-15 00:28:44 -0500

    pulling dynamic board list

--- public/js/app.sideMenu.controller.js
+++ public/js/app.sideMenu.controller.js
@@ -2,9 +2,15 @@ angular
 	.module('app')
 	.controller('sideMenuCtrl', sideMenuCtrl)
 
-	sideMenuCtrl.$inject = ['$scope','authService','profileService'];
+	sideMenuCtrl.$inject = ['$scope','authService','profileService','boardService','$http'];
 
-function sideMenuCtrl ($scope, authService, profileService) {
+function sideMenuCtrl ($scope, authService, profileService, boardService, $http) {
 	vm = this;
-	vm.title = "sideMenuvva"
+	vm.title = "My Boards";
+
+	vm.boards = $http.post('http://localhost:8080/api/boards/byEmail',{owner:"muz@gmail.com"}).then(function(res){
+		vm.boards = res.data
+	});
+
+
 }
